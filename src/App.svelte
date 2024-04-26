30e7abc0dfc5e4b93093dbce1dcf050395a8da64
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-04-26 15:56:11 +0200

    update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -28,7 +28,7 @@
     <style>
       footer {
         background-color: #111827;
-        height: 150px;
+        height: 100px;
       }
       .nav-bar {
         background-color: #111827;
@@ -46,7 +46,7 @@
         color: #2c3e50;
       }
       .icons {
-        margin-top: -10px;
+        margin-top: 10px;
         font-size: 20px;
       }
       .icon {
@@ -62,10 +62,10 @@
   <Route path="/about"><About/></Route>
   <Route path="/contact"><Contact/></Route>
   <Route path="/projects"><Projects/></Route>
-    <Route path="/skills"><Skills/></Route>
+  <Route path="/skills"><Skills/></Route>
   <Route path="/education"><Education/></Route>
 
-    <footer class="text-center p-3">
+    <footer class="text-center">
       <div class="border-t-2 border-white-100 mx-[20%] mb-5"></div>
       <h3 class="text-2xl mb-5 text">Connect with me</h3>
       <div class="p-3  icons">
