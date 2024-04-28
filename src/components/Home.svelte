43c789b25e3373c832591fe545987fc0ccf8262e
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-04-28 13:26:51 +0200

    update Home.svelte and education.svelte

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,3 +1,28 @@
+
+
+<div class="background-container text-center container">
+  <div class="">
+    <div class="">
+      <h1 class="mb-4 text-center text-4xl home-text">
+        Hi there👋
+      </h1>
+      <p class="font-bold font-general-semibold text-2xl  name">I am Tafadzwa Wilson Sedze</p>
+      <span class="text-xl home-text">A Full-Stack Software Developer </span>
+    </div>
+
+    <div class="home-btns">
+      <div class="mt-8">
+      <a href="projects/" class="p-3 bg-slate-800 text-white rounded mb-6 btn"><span class="bi bi-cloud-arrow-down-fill"></span> Download CV </a>
+    </div>
+    <div class="mt-8">
+    <a href="projects/" class="p-3 bg-slate-800 text-white rounded mb-6 btn"><span class="bi bi-briefcase-fill"></span>My Projects</a>
+  </div>
+  </div>
+  
+    </div>
+    
+</div>
+
 <style>
   .background-container {
     background-image: url("../../public/images/projects/background (5).jpg");
@@ -33,26 +58,3 @@
     color: #111827;
   }
 </style>
-
-<div class="background-container text-center container">
-  <div class="">
-    <div class="">
-      <h1 class="mb-4 text-center text-4xl home-text">
-        Hi there👋
-      </h1>
-      <p class="font-bold font-general-semibold text-2xl  name">I am Tafadzwa Wilson Sedze</p>
-      <span class="text-xl home-text">A Full-Stack Developer </span>
-    </div>
-
-    <div class="home-btns">
-      <div class="mt-8">
-      <a href="projects/" class="p-3 bg-slate-800 text-white rounded mb-6 btn"><span class="bi bi-cloud-arrow-down-fill"></span> Download CV </a>
-    </div>
-    <div class="mt-8">
-    <a href="projects/" class="p-3 bg-slate-800 text-white rounded mb-6 btn"><span class="bi bi-briefcase-fill"></span>My Projects</a>
-  </div>
-  </div>
-  
-    </div>
-    
-</div>
--- src/components/education.svelte
+++ src/components/education.svelte
@@ -1,10 +1,25 @@
+<div class="container">
+  <div class="my-education">
+    <h1>My <span>Education</span></h1>
+
+    <a href="https://msuas.ac.zw/" target="_blank" class="head">Bachelor of Science Computer Science Honors</a>
+    <p>Manicaland State University of Applied Sciences</p>
+    <p class="uni">2022 - Present (4 years)</p>
+
+    <a href="https://www.alxafrica.com/" target="_blank" class="head">Full Stack Software Engineering Course</a>
+    <p>ALX Africa</p>
+    <p>Status - Inprogress</p>
+    <p class="last">2023 - Present (9 months )</p>
+    <a href="" target="_blank" class="alx-current">View my Current Transcript</a>
+  </div>
+</div>
+
 <style>
   .container {
     background-color: lightblue;
     max-width: 100%;
     padding-top: 2rem;
     padding-bottom: 2rem;
-
   }
   .my-education {
     padding: 30px;
@@ -23,26 +38,24 @@
     font-weight: bold;
     margin-bottom: 50px;
   }
-  h2 {
+  .head {
     font-size: 24px;
     color: #3182ce;
+    cursor: pointer;
   }
   .uni {
     margin-bottom: 20px;
   }
+  .last {
+    margin-bottom: 10px;
+  }
+  .alx-current {
+    color: #fff;
+    background-color: #3182ce;
+    padding: 5px;
+    border-radius: 5px;
+  }
+  .alx-current:hover {
+    opacity: 0.8;
+  }
 </style>
-
-<div class="container">
-  <div class="my-education">
-    <h1>My <span>Education</span></h1>
-
-    <h2>Bachelor of Science Computer Science Honors</h2>
-    <p>Manicaland State University of Applied Sciences</p>
-    <p class="uni">2022 - Present (4 years)</p>
-
-    <h2>Full Stack Software Engineering Course</h2>
-    <p>ALX Africa</p>
-    <p>Status - Inprogress</p>
-    <p>2023 - Present (9 months )</p>
-  </div>
-</div>
